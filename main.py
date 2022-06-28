9d64d83cf5f252bea4260b7abcf02d9f2e02ba14
Jefferson <jeffersonmontilla.98@gmail.com>
2022-06-27 21:43:39 -0400

    Set up and make a test with selenium library

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+
+
 def main():
-    pass
+    service = Service('./chromedriver')
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get('https://www.google.com')
+    driver.find_element(By.NAME, 'q').send_keys('Selenium', Keys.RETURN)
+    driver.quit()
 
 
 if __name__ == '__main__':
