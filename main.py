b06d2c9cbad1fbde3f458e3b202e48a5b8c6c14f
Jefferson <jeffersonmontilla.98@gmail.com>
2022-06-28 22:01:16 -0400

    Delete the test to separate functionalities

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-
-
 def main():
-    service = Service('./chromedriver')
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get('https://www.google.com')
-    driver.find_element(By.NAME, 'q').send_keys('Selenium', Keys.RETURN)
-    driver.quit()
+    pass
 
 
 if __name__ == '__main__':
