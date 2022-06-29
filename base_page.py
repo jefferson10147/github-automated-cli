57cc5835c134e744a1079ab45cb14494cf5f2279
Jefferson <jeffersonmontilla.98@gmail.com>
2022-06-28 22:11:37 -0400

    Create a file base page for the Selenium logic

---
+++ base_page.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+
+class BasePage:
+    pass
