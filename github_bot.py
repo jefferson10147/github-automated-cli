8454ee89fd7c221635e9d44b1a5d81bef8023342
Jefferson <jeffersonmontilla.98@gmail.com>
2022-07-04 23:36:06 -0400

    Create the init method for GitHub's

--- github_bot.py
+++ github_bot.py
@@ -1,5 +1,8 @@
 from base_page import BasePage
 
 
-class GitHubBot():
-    pass
+class GitHubBot(BasePage):
+    main_page_url = 'https://github.com/'
+
+    def __init__(self):
+        super().__init__(self.main_page_url)
