83bfd94f74ba3e7762886f02405e2d8a9ba8c47a
Jefferson <jeffersonmontilla.98@gmail.com>
2022-08-28 22:52:44 -0400

    Selenium bot: create the function to click in the sign in button

--- github_bot.py
+++ github_bot.py
@@ -1,8 +1,14 @@
+from selenium.webdriver.common.by import By
 from base_page import BasePage
 
 
 class GitHubBot(BasePage):
     main_page_url = 'https://github.com/'
+    signin_btn = (By.CSS_SELECTOR ,'a[href="/login"]')
+
 
     def __init__(self):
         super().__init__(self.main_page_url)
+
+    def click_on_sigin_btn(self):
+        self.click(*self.signin_btn)
