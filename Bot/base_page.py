59b5a1227e4b48e2fd72b81b0c776a9035800eb3
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-15 20:54:32 -0400

    Create functions to close the test and maximize the window on Base page class

---
+++ Bot/base_page.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+
+
+class BasePage:
+
+    def __init__(self, driver_path, url):
+        service = Service(driver_path)
+        options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.implicitly_wait(10)
+        self.wait = WebDriverWait(self.driver, 10)
+        self.main_page = url
+
+    def open_main_page(self):
+        self.driver.get(self.main_page)
+
+    def end_test(self):
+        self.driver.quit()
+
+    def maximize_window(self):
+        self.driver.maximize_window()
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    def click(self, *locator):
+        self.driver.find_element(*locator).click()
+
+    def input_text(self, text, *locator):
+        element = self.driver.find_element(*locator)
+        element.clear()
+        element.send_keys(text)
+
+    def wait_for_element_click(self, *locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
