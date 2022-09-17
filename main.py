b4b4bda272fe2ee3b99cb9153cf888e084b6f8e3
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-17 09:27:43 -0400

    Add the cli

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
+from re import A
 from Tests.bot_tests import create_repository_test
+
+
+def cli():
+    parser = argparse.ArgumentParser(description='GitHub CLI tool')
+    parser.add_argument(
+        '-c', '--create', action='store_true', help='Creates a repository')
+    parser.add_argument('-n', '--name', help='Repository name')
+    parser.add_argument(
+        '-p', '--private', action='store_true', help='Flag to make the repository private')
 
 
 def main():
