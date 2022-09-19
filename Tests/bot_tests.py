eb00c298fdc35ec079dafc63ff2373861b3ca7e8
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-18 21:10:13 -0400

    Use the new function from the bot to make the test

--- Tests/bot_tests.py
+++ Tests/bot_tests.py
@@ -4,15 +4,4 @@ from Bot.github_bot import GitHubBot
 def create_repository_test():
     repository_name = 'test-repo'
     bot = GitHubBot()
-    bot.open_main_page()
-    bot.maximize_window()
-    bot.click_on_sigin_btn()
-    bot.enter_user()
-    bot.enter_password()
-    bot.submit_sigin_data()
-    bot.click_on_more_options_btn()
-    bot.click_on_new_repository()
-    bot.enter_respository_name(repository_name)
-    bot.set_private_repository()
-    bot.create_repository()
-    bot.end_test()
+    bot.create_repository(repository_name)
