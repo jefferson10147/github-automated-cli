b9366c4dfb2ff32602c0cd5b5f9ae01fc406ac84
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-19 21:20:59 -0400

    Bot: add some documentation to the methods using pythnon conventions

--- Bot/github_bot.py
+++ Bot/github_bot.py
@@ -20,46 +20,46 @@ class GitHubBot(BasePage):
     create_repository_btn = (By.CSS_SELECTOR, '.btn-primary.btn')
 
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.username = Config.username
         self.password = Config.password
         self.driver_path = Config.driver_path
         super().__init__(self.driver_path, self.main_page_url)
 
-    def click_on_sigin_btn(self):
+    def click_on_sigin_btn(self) -> None:
         self.wait_for_element_click(*self.signin_btn)
 
-    def enter_user(self):
+    def enter_user(self) -> None:
         self.input_text(self.username, *self.user_field)
 
-    def enter_password(self):
+    def enter_password(self) -> None:
         self.input_text(self.password, *self.password_field)
 
-    def submit_sigin_data(self):
+    def submit_sigin_data(self) -> None:
         self.wait_for_element_click(*self.sigin_submit)
 
-    def click_on_more_options_btn(self):
+    def click_on_more_options_btn(self) -> None:
         self.wait_for_element_click(*self.more_options_btn)
 
-    def click_on_new_repository(self):
+    def click_on_new_repository(self) -> None:
         self.wait_for_element_click(*self.new_repository_dropdown_btn)
 
-    def enter_respository_name(self, repository_name):
+    def enter_respository_name(self, repository_name) -> None:
         self.input_text(repository_name, *self.input_repository_name)
 
-    def enter_repository_description(self, description):
+    def enter_repository_description(self, description) -> None:
         self.input_text(description, *self.repository_description)
 
-    def set_private_repository(self):
+    def set_private_repository(self) -> None:
         self.wait_for_element_click(*self.private_repository_option)
 
-    def set_public_repository(self):
+    def set_public_repository(self) -> None:
         self.wait_for_element_click(*self.public_respository_option)
 
-    def create_repository(self):
+    def create_repository(self) -> None:
         self.wait_for_element_click(*self.create_repository_btn)
 
-    def start_repository(self, repository_name):
+    def start_repository(self, repository_name: str) -> bool:
         print(f'Creating repository called {repository_name}...')
         self.open_main_page()
         self.maximize_window()
@@ -73,4 +73,4 @@ class GitHubBot(BasePage):
         self.set_private_repository()
         self.create_repository()
         self.end_test()
-        print('The repository has been created :D')
+        print('The repository has been created')
