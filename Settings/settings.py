b0f66091ebc4abf716a8c34467d6128f78090fbf
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-20 22:23:24 -0400

    Settings: add the github link

--- Settings/settings.py
+++ Settings/settings.py
@@ -9,3 +9,5 @@ class Config:
     password = config('PASSWORD')
     driver_path = './chromedriver'
     local_path = '/Users/jeffersonmontilla/Documents'
+    github_link = f'https://github.com/{username}/'
+    main_branch_name = 'master'
