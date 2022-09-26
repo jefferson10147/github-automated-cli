fbdf3588efe041863038fd6a923fd08a11f2ca6f
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-25 23:06:17 -0400

    Bot: click on the button to confirm the delete action

--- Bot/github_bot.py
+++ Bot/github_bot.py
@@ -31,10 +31,6 @@ class GitHubBot(BasePage):
     your_repositories_drop_down = (By.CSS_SELECTOR, 'a[href="/jefferson10147?tab=repositories"]')
     settings_btn = (By.ID, 'settings-tab')
     delete_respository_btn = (By.XPATH, '//summary[contains(text(), "Delete this repository")]')
-    delete_repository_confirm_btn = (
-        By.CSS_SELECTOR, 
-        'form[action="/jefferson10147/test/settings/delete"] button.btn-danger.btn.btn-block'
-    )
 
 
     def __init__(self) -> None:
@@ -116,3 +112,10 @@ class GitHubBot(BasePage):
             f'form[action="/{confirmation_input}/settings/delete"] p input[name="verify"]'
         )
         self.input_text(confirmation_input, *delete_repository_input)
+
+    def click_on_delete_repository_confirm_btn(self, confirmation_input):
+        delete_repository_confirm_btn = (
+            By.CSS_SELECTOR, 
+            f'form[action="/{confirmation_input}/settings/delete"] button.btn-danger.btn.btn-block'
+        )
+        self.wait_for_element_click(*delete_repository_confirm_btn)
