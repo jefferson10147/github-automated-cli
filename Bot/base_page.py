65bd4e6e0d2dcbb0b6cfc535822ba422a178f821
Jefferson <jeffersonmontilla.98@gmail.com>
2022-09-29 23:52:53 -0400

    Delete some unnecessary code

--- Bot/base_page.py
+++ Bot/base_page.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 
 
 class BasePage:
